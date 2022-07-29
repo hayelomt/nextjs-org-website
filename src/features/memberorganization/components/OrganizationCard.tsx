19c7575067408979b81abdbb02ca88041dc92bde
Hayelom Tizazu <hayelomtizazu19@gmail.com>
2022-07-29 10:08:54 +0300

    organization fix

--- src/features/memberorganization/components/OrganizationCard.tsx
+++ src/features/memberorganization/components/OrganizationCard.tsx
@@ -1,7 +1,13 @@
+import { motion } from 'framer-motion';
+
 const OrganizationCard = () => {
   return (
     <>
-      <div className="h-[170px] w-full xs:w-[284px] max-w-[284px] bg-brand rounded-[15px]"></div>
+      <motion.div
+        whileHover={{ y: -15, scale: 1.01, transition: { stiffness: 80 } }}
+        animate={{ y: 0, scale: 1, transition: { stiffness: 80 } }}
+        className="h-[170px] w-full xs:w-[284px] max-w-[284px] bg-brand rounded-[15px]"
+      ></motion.div>
     </>
   );
 };
